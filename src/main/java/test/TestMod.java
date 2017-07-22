5141c7fb084b8f8f4854629d478df0f6e68bb6a5
Mybaum <mybaum@outlook.de>
2017-07-23 01:30:57 +0200

    Initial commit

---
+++ src/main/java/test/TestMod.java
@@ -0,0 +1,18 @@
+package test;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TestMod.MODID, version = TestMod.VERSION)
+public class TestMod {
+    public static final String MODID = "testmod";
+    public static final String VERSION = "0.1";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
+    }
+}
